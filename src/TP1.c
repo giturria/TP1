c0990b2f6b674b3f9428520584d0cec7e24536df
German <German@DESKTOP-LKBFDDO>
2018-04-25 07:39:45 -0300

    Agregado parte 2 y 3

--- src/TP1.c
+++ src/TP1.c
@@ -35,10 +35,37 @@
  * Date: 2016-04-26
  */
 
+#define COMPILAR_TP1-1  /*parpadeo del LEDs*/
+#define COMPILAR_TP1-2  /*sensado de Push Buttons*/
+#define COMPILAR_TP1-3  /*uso de tickHooks*/
+
+#define COMPILAR COMPILAR_TP1-2
+
+
 /*==================[inclusions]=============================================*/
 
-#include "TP1.h"   // <= own header (optional)
+/*#include "TP1-1.h"  */  // <= own header (optional)
 #include "sapi.h"       // <= sAPI header
+
+#if COMPILAR==COMPILAR_TP1-3
+    #include "TP1-3.h"
+    /* FUNCION que se ejecuta cada vez que ocurre un Tick. */
+void myTickHook( void *ptr ){
+
+   static bool_t ledState = OFF;
+
+   gpioMap_t led = (gpioMap_t)ptr;
+
+   if( ledState ){
+      ledState = OFF;
+   }
+   else{
+      ledState = ON;
+   }
+   gpioWrite( led, ledState );
+}
+#endif
+
 
 /*==================[macros and definitions]=================================*/
 
@@ -62,8 +89,43 @@ int main(void){
    /* Inicializar la placa */
    boardConfig();
 
+
+   /* ------------- uso de tickHooks ------------- */
+#if COMPILAR==COMPILAR_TP1-3
+   /* Inicializar el conteo de Ticks con resolucion de 50ms (se ejecuta
+      periodicamente una interrupcion cada 50ms que incrementa un contador de
+      Ticks obteniendose una base de tiempos). */
+   tickConfig( 50 );
+
+   /* Se agrega ademas un "tick hook" nombrado myTickHook. El tick hook es
+      simplemente una funcion que se ejecutara peri�odicamente con cada
+      interrupcion de Tick, este nombre se refiere a una funcion "enganchada"
+      a una interrupcion.
+      El segundo parametro es el parametro que recibe la funcion myTickHook
+      al ejecutarse. En este ejemplo se utiliza para pasarle el led a titilar.
+   */
+
+   tickCallbackSet( myTickHook, (void*)LEDR );
+   delay(1000);
+
+   /* ------------- sensado de Push Buttons ------------- */
+
+#elif COMPILAR==COMPILAR_TP1-2
+
+   gpioConfig( GPIO0, GPIO_INPUT );
+
+      gpioConfig( GPIO1, GPIO_OUTPUT );
+
+      /* Variable para almacenar el valor de tecla leido */
+      bool_t valor;
+
+#endif
+
    /* ------------- REPETIR POR SIEMPRE ------------- */
    while(1) {
+
+   /* -------------parpadeo del LEDs ------------- */
+#if COMPILAR==COMPILAR_TP1-1
 
       /* Prendo el led azul */
       gpioWrite( LEDB, ON );
@@ -74,6 +136,42 @@ int main(void){
       gpioWrite( LEDB, OFF );
 
       delay(500);
+
+      /* ------------- sensado de Push Buttons ------------- */
+#elif COMPILAR==COMPILAR_TP1-2
+
+            valor = !gpioRead( TEC1 );
+            gpioWrite( LEDB, valor );
+
+            valor = !gpioRead( TEC2 );
+            gpioWrite( LED1, valor );
+
+            valor = !gpioRead( TEC3 );
+            gpioWrite( LED2, valor );
+
+            valor = !gpioRead( TEC4 );
+            gpioWrite( LED3, valor );
+
+            valor = !gpioRead( GPIO0 );
+            gpioWrite( GPIO1, valor );
+
+       /* ------------- uso de tickHooks ------------- */
+ #elif COMPILAR==COMPILAR_TP1-3
+
+            tickCallbackSet( myTickHook, (void*)LEDG );
+            delay(1000);
+            tickCallbackSet( myTickHook, (void*)LEDB );
+            delay(1000);
+            tickCallbackSet( myTickHook, (void*)LED1 );
+            delay(1000);
+            tickCallbackSet( myTickHook, (void*)LED2 );
+            delay(1000);
+            tickCallbackSet( myTickHook, (void*)LED3 );
+            delay(1000);
+            tickCallbackSet( myTickHook, (void*)LEDR );
+            delay(1000);
+
+#endif
 
    }
 
